080ab661f202a0c20125fe6589ddcaf3e10124bf
kamann097 <contactamankumar1991@gmail.com>
2020-08-06 15:53:41 +0530

    Completed the serach field

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+import React from 'react';
+// import Accordion from "./components/Accordion";
+import Search from './components/Search';
 
-const items = [
-  {
-    title: "what is react?",
-    content: "React is a front-end framework",
-  },
-  {
-    title: "Why use React?",
-    content: "React is a famous JS library amoung enginners",
-  },
-  {
-    title: "How do you use React?",
-    content: "Use React to reach",
-  },
-];
+// const items = [
+//   {
+//     title: 'what is react?',
+//     content: 'React is a front-end framework',
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a famous JS library amoung enginners',
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'Use React to reach',
+//   },
+// ];
 
 const App = () => {
   return (
